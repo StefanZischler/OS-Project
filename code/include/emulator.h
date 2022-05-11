a268b83524f1254e61d1561a0fba0e7a5f0cd034
sebastianLukas1 <sebastian.lukas@stud.unibas.ch>
2022-05-11 07:28:58 +0200

    added exit context to emulator

--- code/include/emulator.h
+++ code/include/emulator.h
@@ -4,10 +4,11 @@
 typedef struct {
   bool paused;
   bool running;
+  bool exit;
 } emu_context;
 
 int emulator_run(int argc, char **argv);
 
-emu_context* emulator_get_context();
+emu_context *emulator_get_context();
 
 #endif
