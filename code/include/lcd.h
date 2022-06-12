d3ff442899ff9007cb4b43991fc7563db040700b
StefanZischler <54356772+StefanZischler@users.noreply.github.com>
2022-06-12 21:24:38 +0200

    finished LCD

--- code/include/lcd.h
+++ code/include/lcd.h
@@ -3,22 +3,84 @@
 
 #include <define.h>
 
+/*
+LCD Display: Manage display, pixel colors, windows, status, etc.
+
+*/
 typedef struct {
+  //registers
   u8 lcd_control;
   u8 status_register;
-  
+  //draw pixel line information
   u8 scroll_y;
   u8 scroll_x;
   u8 line_y;
   u8 line_y_compare;
+  //color palettes
   u8 background_palette;
   u8 object_palette_0;
   u8 object_palette_1;
+  //window
   u8 window_y;
   u8 window_x;
+  //pixel colors (4 grey tones)
+  u32 background_colors[4];
+  u32 sprite_1_color[4];
+  u32 sprite_2_color[4];
 } lcd_context;
 
 lcd_context* lcd_get_context();
+
+
+/* 
+FF40 LCD Control (LDLC) https://gbdev.io/pandocs/LCDC.html
+7	LCD and PPU enable	0=Off, 1=On
+6	Window tile map area	0=9800-9BFF, 1=9C00-9FFF
+5	Window enable	0=Off, 1=On
+4	BG and Window tile data area	0=8800-97FF, 1=8000-8FFF
+3	BG tile map area	0=9800-9BFF, 1=9C00-9FFF
+2	OBJ size	0=8x8, 1=8x16
+1	OBJ enable	0=Off, 1=On
+0	BG and Window enable/priority	0=Off, 1=On
+*/
+
+//helper define to get right bit for LCDC 
+#define FIND_BIT(target, offset) ((target & (1 << offset)) ? 1 : 0)
+#define LDC_PPU_ENABLE (FIND_BIT(lcd_get_context()->lcd_control, 7))
+#define LDC_TILE_MAP (FIND_BIT(lcd_get_context()->lcd_control, 6) ? 0x9C00 : 0x9800)
+#define LDC_WINDOW_ENABLE (FIND_BIT(lcd_get_context()->lcd_control, 5))
+#define LDC_BG_WINDOW_TILE (FIND_BIT(lcd_get_context()->lcd_control, 4) ? 0x8000 : 0x8800)
+#define LDC_TILE_AREA (FIND_BIT(lcd_get_context()->lcd_control, 3) ? 0x9C00 : 0x9800)
+#define LDC_OBJ_SIZE (FIND_BIT(lcd_get_context()->lcd_control, 2) ? 16 : 8)
+#define LDC_OBJ_ENABLE (FIND_BIT(lcd_get_context()->lcd_control, 1))
+#define LDC_PRIORITY_BG_WINDOW (FIND_BIT(lcd_get_context()->lcd_control, 0))
+
+/*
+FF41 - STAT (LCD Status) (R/W) (https://gbdev.io/pandocs/STAT.html)
+Bit 6 - LYC=LY STAT Interrupt source         (1=Enable) (Read/Write)
+Bit 5 - Mode 2 OAM STAT Interrupt source     (1=Enable) (Read/Write)
+Bit 4 - Mode 1 VBlank STAT Interrupt source  (1=Enable) (Read/Write)
+Bit 3 - Mode 0 HBlank STAT Interrupt source  (1=Enable) (Read/Write)
+Bit 2 - LYC=LY Flag                          (0=Different, 1=Equal) (Read Only)
+Bit 1-0 - Mode Flag                          (Mode 0-3, see below) (Read Only)
+          0: HBlank
+          1: VBlank
+          2: Searching OAM
+          3: Transferring Data to LCD Controller
+*/ 
+typedef enum {
+  STATUS_LYC = (1 << 6),
+  STATUS_OAM = (1 << 5),
+  STATUS_VBLANK = (1 << 4),
+  STATUS_HBLANK = (1 << 3)
+} lcd_status;
+
+#define LCDS_STATUS_INTERRUPTS(interrupt) (lcd_get_context()->status_register & interrupt)
+
+//helper function to set bit for LYC
+#define SET_BIT (target, offset, bit) {if (bit) target |= (1 << offset); else target &= ~(1 << offset);}
+#define LCD_LYC (bit) (SET_BIT(lcd_get_context()->status_register, 2, bit))
+
 
 void lcd_init();
 
--- code/lib/rendering/lcd.c
+++ code/lib/rendering/lcd.c
@@ -1,4 +1,5 @@
 #include <lcd.h>
+#include <dma.h>
 
 static lcd_context ctx;
 
@@ -6,20 +7,34 @@ lcd_context* lcd_get_context() {
   return &ctx;
 }
 
+//inizialize standart color palette of GameBoy (hexcolors)
+//white = 0xFFFFFFFF (#FFFFFF), grey = 0xFFAAAAAA (#AAAAAA), 
+//dark grey = 0xFF555555 (#555555), black = 0xFF000000 (#000000)
+static unsigned long standart_colors[4] = {0xFFFFFFFF, 0xFFAAAAAA, 0xFF555555, 0xFF000000};
 //initialization, values taken from
 //gbdev.io/pandocs/Power_Up_Sequence.html#hardware-registers
 void lcd_init() {
+  //register
   ctx.lcd_control = 0x91;
-  ctx.status_register = 0x81;	//value not the same for every bootloader
+  ctx.status_register = 0x81;	
+  //draw pixel line information
   ctx.scroll_y = 0x00;
   ctx.scroll_x = 0x00;
   ctx.line_y = 0x91;
   ctx.line_y_compare = 0x00;
+  //color palettes
   ctx.background_palette = 0xFC;
-  ctx.object_palette_0 = 0x00;	//actual start value unknown
-  ctx.object_palette_1 = 0x00;
+  ctx.object_palette_0 = 0xFF;	
+  ctx.object_palette_1 = 0xFF;
+  //window
   ctx.window_y = 0x00;
   ctx.window_x = 0x00;
+  // inizilite pixel colors (4 grey tones)
+  for (int i = 0; i < 4; i++) {
+    ctx.background_colors[i] = standart_colors[i];
+    ctx.sprite_1_color[i] = standart_colors[i];
+    ctx.sprite_2_color[i] = standart_colors[i];
+  }
 }
 
 u8 lcd_read(u16 address) {
@@ -37,7 +52,7 @@ u8 lcd_read(u16 address) {
     case 0xFF45:
       return ctx.line_y_compare;
     case 0xFF46:
-      //TODO: dma
+      //DMA only needs write
       break;
     case 0xFF47:
       return ctx.background_palette;
@@ -50,6 +65,30 @@ u8 lcd_read(u16 address) {
     case 0xFF4B:
       return ctx.window_x;
   }
+}
+
+//update the color palletes
+void update_color_palette(u8 palette_colors, u8 palette) {
+  //get backround palette colors
+  u32 *colors = ctx.background_colors;
+
+  //switch depending which palette used
+  switch (palette)
+  {
+  case 1:
+    colors = ctx.sprite_1_color;
+    break;
+  
+  case 2:
+  colors = ctx.sprite_2_color;
+    break;
+  }
+
+  //update palette (at the right bit)
+  colors[0] = standart_colors[palette_colors & 0b11];
+  colors[1] = standart_colors[(palette_colors >> 2) & 0b11];
+  colors[2] = standart_colors[(palette_colors >> 4) & 0b11];
+  colors[3] = standart_colors[(palette_colors >> 6) & 0b11];
 }
 
 void lcd_write(u16 address, u8 value) {
@@ -73,16 +112,18 @@ void lcd_write(u16 address, u8 value) {
       ctx.line_y_compare = value;
       break;
     case 0xFF46:
-      //TODO: dma
-      break;
+      dma_beginning(value);
     case 0xFF47:
-      ctx.background_palette = value;
+    //update backround color palette
+      update_color_palette(value, 0);
       break;
     case 0xFF48:
-      ctx.object_palette_0 = value;
+    //update color palette (besides last two bit, since they are transparent)
+      update_color_palette(value & 0b11111100, 1);
       break;
     case 0xFF49:
-      ctx.object_palette_1 = value;
+    //update color palette (besides last two bit, since they are transparent)
+      update_color_palette(value & 0b11111100, 2);
       break;
     case 0xFF4A:
       ctx.window_y = value;
